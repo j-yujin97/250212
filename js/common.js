c47815f56cb8b732f41a7ec98e05bf06bc190bab
j-yujin97 <jeongyj9799@gmail.com>
2025-02-12 10:07:29 +0900

    알럿

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
-window.addEventListener("DOMContentLoaded", function(){})
+window.addEventListener("DOMContentLoaded", function(){
+     const allmenu = document.querySelector("#btn .me");
+    allmenu.addEventListener("click",function(){
+    alert("2번만")
+     })
+
+    
+    // const allmenu = document.getElementById("allmenu");
+    // allmenu.addEventListener("click",function(){
+    //     alert("버튼클릭뱃지")
+    // })
+
+})
+
+
